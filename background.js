0e5288a7091d715afe94346e3faa384e42630ee7
unknown <vishal.vikal.88@gmail.com>
2021-03-27 09:43:09 +0530

    clean background.js

--- background.js
+++ background.js
@@ -1,22 +1,3 @@
-// console.log("Bacground is running");
-// chrome.browserAction.onClicked.addListener(buttonClicked);
-
-// function buttonClicked(tab){
-//   console.log(chrome.extension.getURL('ct.png'));
-//   chrome.system.cpu.getInfo((info, info1) => console.log(info, info1));
-//   chrome.system.storage.getInfo(info=>console.log(info));
-//   chrome.system.memory.getInfo(info=>console.log(info));
-//   chrome.system.display.getInfo(info=>console.log(info));
-//   let msg ={
-//     txt:"hello"
-//   }
-//   chrome.tabs.sendMessage(tab.id, msg);
-// }
-
-// window.addEventListener('message', function(event){
-//   console.log(event);
-// })
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   if(request.request === 'GetSystemStatus'){
       chrome.system.cpu.getInfo(cpuInfo=>{
